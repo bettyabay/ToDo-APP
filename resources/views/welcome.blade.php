1e9af9f86afaefd822aa690494ee0533e522928e
bettyabay <88900241+bettyabay@users.noreply.github.com>
2024-11-13 17:01:17 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        
+        <!-- Fonts -->
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        
+    </head>
+    <body class="bg-gray-200 p-4" >
+        <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl">
+
+        </div>
+    </body>
+</html>
